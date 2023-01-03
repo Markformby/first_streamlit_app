036e8134d478baf021e45a288e2b2a2b8606ec2a
Markformby <121609736+Markformby@users.noreply.github.com>
2023-01-03 07:19:42 +0000

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -7,6 +7,8 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avacado Toast')
+
 
 streamlit.header('(apple) Build Your Own Fruit Smoothie')
 
